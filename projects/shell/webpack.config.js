f81ad15b78b17d678b16422d46e32b7d51d9e406
Alain Boudard <coco.boudard@gmail.com>
2023-06-18 23:07:25 +0200

    init

---
+++ projects/shell/webpack.config.js
@@ -0,0 +1,19 @@
+const { shareAll, withModuleFederationPlugin, share } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "mfe1": "http://localhost:4201/remoteEntry.js",
+  },
+
+  /* shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  }, */
+  // Explicitly share packages:
+  shared: share({
+    "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+}),
+
+});
